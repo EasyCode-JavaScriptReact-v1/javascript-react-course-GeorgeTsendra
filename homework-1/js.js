e8ca5faf5a223034345e1cf7499271d7f753633f
George Tsendra <georetsendrafreelan@mail.ru>
2018-05-31 22:08:33 +0300

    Add files via upload

---
+++ homework-1/js.js
@@ -0,0 +1,21 @@
+
+const user = {
+  name: "Petia",
+
+};
+
+const alexMassagers = ["Facebook", "vk", "Telegram"];
+
+const contact = {
+
+ifHavePict: true,
+name: "Alexander",
+famale: undefined,
+workNumb: null,
+number: 0502724890,
+masagers: alexMassagers,
+commonFriends: user,
+
+};
+
+console.log(contact);
